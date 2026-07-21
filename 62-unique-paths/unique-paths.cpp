5df1ee2b7ca7f8400e646247edf89d65c900249f
Bhavesh <142612199+bhaveshpant321@users.noreply.github.com>
2026-07-21 15:20:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (82.64%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        // Using DP: Optimized for O(n) space
-        vector<int> row(n, 1);  // 1 way to move in every column in the first row
-
+        // Combinatorics solution: (m+n-2)!/(m-1)!(n-1)!
+        // Optimal solution: DP: Num ways to reach the bottom: Either top or left
+        
+        vector<int> res(n, 1);
         for(int i=1; i<m; i++){
             for(int j=1; j<n; j++){
-                row[j]= row[j]+row[j-1];    // paths[i,j]= paths[i-1,j]+paths[i,j-1]
+                res[j]+= res[j-1];
             }
         }
-        return row[n-1];
+        return res[n-1];
     }
 };
