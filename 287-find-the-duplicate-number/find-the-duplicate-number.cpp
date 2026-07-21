24d2c4aad65d8a3a795801664b3eafdc518001f2
Bhavesh <142612199+bhaveshpant321@users.noreply.github.com>
2026-07-21 15:01:58 +0530

    Time: 4 ms (60.55%) | Memory: 65.2 MB (41.91%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        // Floyd's tortoise and hare
-        int slow = nums[0];
-        int fast = nums[0];
+        // Can be optimally done using Floyd's (learn the approach tho)
 
-        do {
-            slow = nums[slow];
-            fast = nums[nums[fast]];
-        } while (slow != fast);
-
-        fast = nums[0];
-        while (slow != fast) {
-            slow = nums[slow];
-            fast = nums[fast];
+        int slow= nums[0], fast= nums[0];
+        
+        while(true){
+            slow= nums[slow];
+            fast= nums[nums[fast]];
+            if(slow==fast) break;
         }
-        return slow;  // or nums[slow]
+        int slow2= nums[0];
+        while(slow!=slow2){
+            slow= nums[slow];
+            slow2= nums[slow2];
+        }
+        return slow;
+
+        // Approach 2: Binary search: take the mid, count how many values less than mid (nlogn). Pigeonhole principle: If a number repeats itself in a continuous range 1-n, ther will be n+1 numbers (making that our search space), if numbers are n, no number is repeating
     }
 };
