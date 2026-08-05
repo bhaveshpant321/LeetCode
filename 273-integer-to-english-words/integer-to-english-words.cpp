6d392e42fab333b7d5a138007ca86aa572ef9622
Bhavesh <142612199+bhaveshpant321@users.noreply.github.com>
2026-08-05 14:59:37 +0530

    Time: 2 ms (57.39%) | Memory: 9.9 MB (81.45%) - LeetSync

--- 273-integer-to-english-words/integer-to-english-words.cpp
+++ 273-integer-to-english-words/integer-to-english-words.cpp
@@ -1,29 +1,30 @@
 class Solution {
-    string belowTwenty[20] = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-    string tens[10] = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-    string thousands[4] = {"", "Thousand", "Million", "Billion"};
+public:
+    // English numbers process in chunks of 3 digits (thousand, million, billion)
+    // Required 3 lookup tables: below 20, tens and thousands scale
+    // Recursive 3 digit helper function needed
+    // Process right to left using %1000 and /1000, apply corresponding scale on non-zero 3 digit chunk
 
-    string solve(int n) {
-        if (n == 0) return "";
-        if (n < 20) return belowTwenty[n] + " ";
-        if (n < 100) return tens[n / 10] + " " + solve(n % 10);
-        return belowTwenty[n / 100] + " Hundred " + solve(n % 100);
+    string thousands[4]= {"", " Thousand", " Million", " Billion"};
+    string tens[10]= {"", " Ten", " Twenty", " Thirty", " Forty", " Fifty", " Sixty", " Seventy", " Eighty", " Ninety"};
+    string ones[20]= {"", " One", " Two", " Three", " Four", " Five", " Six", " Seven", " Eight", " Nine", " Ten", " Eleven", " Twelve", " Thirteen", " Fourteen", " Fifteen", " Sixteen", " Seventeen", " Eighteen", " Nineteen"};
+    
+    // Helper function
+    string helper(int n){
+        if(n<20) return ones[n];
+        if(n<100) return tens[n/10]+ helper(n%10);
+        if(n<1000) return helper(n/100)+ " Hundred" + helper(n%100);
+        // the thousands can be selected on powers of 1000
+        for(int i=3; i>=0; i--){
+            if(n>=pow(1000, i)){
+                return helper(n/pow(1000, i))+thousands[i]+helper(n%(int)pow(1000, i));
+            }
+        }
+        return "";
     }
 
-public:
     string numberToWords(int num) {
-        if (num == 0) return "Zero";
-        string res = "";
-        int i = 0;
-        while (num > 0) {
-            if (num % 1000 != 0) {
-                res = solve(num % 1000) + thousands[i] + " " + res;
-            }
-            num /= 1000;
-            i++;
-        }
-        // Clean up extra spaces (the "Silly Bug" shield)
-        while (res.back() == ' ') res.pop_back();
-        return res;
+        if(num==0) return "Zero";
+        return helper(num).substr(1);   // To remove the trailing first space (every number has space before it in our array)
     }
 };
