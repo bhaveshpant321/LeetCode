4949603152437e32dc1822497f7a582baa928fdc
Bhavesh <142612199+bhaveshpant321@users.noreply.github.com>
2026-08-10 21:43:37 +0530

    Time: 0 ms (100.00%) | Memory: 26.3 MB (36.18%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int low=0, high=nums.size()-1;
-        if(high==0 || nums[low]!=nums[low+1]){
-            return nums[0];
+        // How to identify valid partition: until the element that occurs once, the first occurence of any digit would be even index, thereafter odd
+        int left= 0, right= nums.size()-1;
+        while(left<right){
+            int mid= left+(right-left)/2;
+            if((mid%2==0 && nums[mid]== nums[mid+1]) || (mid%2==1 && nums[mid-1]== nums[mid])){
+                left= mid+1;
+            }else{
+                right= mid;
+            }
         }
-        //The position of second element will tell whether it's the left side or the right
-        while(low<=high){
-            int mid = low + (high - low) / 2;
-            if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) return nums[mid];
-            if ((mid % 2 == 0 && nums[mid] == nums[mid + 1])
-             || (mid % 2 == 1 && nums[mid] == nums[mid - 1])) low = mid +1;
-            else high = mid - 1;
-        }
-        return 0;
+        return nums[left];
     }
 };
