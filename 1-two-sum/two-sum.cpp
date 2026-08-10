8fbe67ea649df2414a1a3025e9149730592d1317
Bhavesh <142612199+bhaveshpant321@users.noreply.github.com>
2026-08-10 19:22:32 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (23.68%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> map;    // target-value, index
-        
+        // Hash map solution
+        unordered_map<int, int> map;    // target-val, index
         for(int i=0; i<nums.size(); i++){
-            if(map.count(nums[i])){
+            if(!map.count(nums[i])){
+                map.insert({target-nums[i], i});
+            }else{
                 return {map[nums[i]], i};
-            }  // we found current value
-            else{
-                map[target-nums[i]]= i;
             }
         }
-        return {-1,-1};
+        return {-1, -1};
     }
 };
