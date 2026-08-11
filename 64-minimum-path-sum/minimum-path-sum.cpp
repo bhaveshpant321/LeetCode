b8b9615298d443afe06a4d7c56036eef8be335ad
Bhavesh <142612199+bhaveshpant321@users.noreply.github.com>
2026-08-11 21:42:22 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (96.28%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        int m= grid.size(); // Row
-        int n= grid[0].size(); // Column
-
-        vector<int> dp(n);
-        dp[0]= grid[0][0];
-        for(int i=1; i< n; i++){
-            dp[i]= grid[0][i]+ dp[i-1]; // Filling the first row. We can only go left to right.
+        // possible moves: Either from up or left
+        // 1D solution: take the first column and take prefix sum
+        int m= grid.size(), n= grid[0].size();
+        vector<int> curr(n, 0);
+        int prev=0;
+        for(int i=0; i<n; i++){
+            curr[i]= prev+grid[0][i];
+            prev= curr[i];
         }
 
-        for(int j=1; j<m; j++){
-            dp[0]+= grid[j][0]; // For the first column, the value would be top+ curr
-            for(int i=1; i<n; i++){
-                dp[i]= grid[j][i]+ min(dp[i], dp[i-1]);
+        // Traverse from row 2
+        for(int i=1; i<m; i++){
+            curr[0]+=grid[i][0];
+            for(int j=1; j<n; j++){
+                curr[j]= grid[i][j]+ min(curr[j], curr[j-1]);
             }
         }
-        return dp[n-1];
+        return curr[n-1];
     }
 };
