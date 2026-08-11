670108c68eb1b8b4f6b5250fff8d74e4a9fcfb6d
Bhavesh <142612199+bhaveshpant321@users.noreply.github.com>
2026-08-11 17:58:50 +0530

    Time: 70 ms (21.73%) | Memory: 43.8 MB (93.75%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -10,14 +10,13 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        // LCA is the lowest node T that has both p and q as the descendants
-        // A node can be a descendant of itself
-        // Approach, if p or q, return, if for a node left and right both return non null, that's the current LCA, else return null
+        // base case
         if(!root || root==p || root==q) return root;
+
         TreeNode* left= lowestCommonAncestor(root->left, p, q);
         TreeNode* right= lowestCommonAncestor(root->right, p, q);
-
+        
         if(left && right) return root;
-        return (!left)? right: left;
+        return left? left: right;
     }
 };
