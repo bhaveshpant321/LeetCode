f2ca94ad3d5d4791d47b99e9ec81774f2a06541e
Bhavesh <142612199+bhaveshpant321@users.noreply.github.com>
2026-08-11 17:43:34 +0530

    Time: 15 ms (98.44%) | Memory: 14.5 MB (90.63%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,68 +1,97 @@
-class DSU{
-    vector<int> parent;
-    vector<int> rank;
-public:
-    DSU(int n){
-        parent.resize(n);
-        rank.assign(n, 0);  // rank is 0 by default
-        for(int i=0; i<n; i++) parent[i]= i;    // each node is it's parent by default
-    }
-
-    int find(int i){
-        if(parent[i]==i) return i;
-        return parent[i]= find(parent[i]); // Path compression
-    }
-
-    bool unite(int i, int j){
-        int root_i= find(i);
-        int root_j= find(j);
-
-        if(root_i!= root_j){
-            // union by rank
-            if(rank[root_i]< rank[root_j]){
-                parent[root_i]= root_j;
-            }else if(rank[root_i]> rank[root_j]){
-                parent[root_j]= root_i;
-            }else{
-                parent[root_j]= root_i;
-                rank[root_i]++;
-            }
-            return true;
-        }
-        return false;
-    }
-};
-
-
 class Solution {
 public:
-    int minCostConnectPoints(vector<vector<int>>& points) {
-        // MST
-        // Either Kruskal's or Prims
-        // MST: sort by weights, use DSU to join
-        vector<tuple<int, int, int>> edgeList;
-        // Treat each coordinate as a point, we need N(N-1)/2 edges
+    int minCostConnectPoints(vector<vector<int>>& a) {
+        // Two approaches: Kruskals (DSU) and Prims (greedy/ min heap)
+        // Using prims
+        int n= a.size(), ans=0;
+        vector<int> d(n, 1e9), vis(n);
+        d[0]=0;
 
-        for(int i=0; i<points.size(); i++){
-            for(int j=i+1; j<points.size(); j++){
-                int dist= abs(points[i][0]- points[j][0])+ abs(points[i][1]- points[j][1]);
-                edgeList.push_back({dist, i, j});   // i and j are indices of coordinates
-            }
+        for(int i=0; i<n; i++){
+            int u=-1;
+            
+            for(int j=0; j<n; j++) if(!vis[j] && (u==-1 || d[j]<d[u])) u=j;
+            
+            vis[u]=1;
+            ans+=d[u];
+
+            for(int v=0; v<n; v++) if(!vis[v]) d[v]= min(d[v], abs(a[u][0]-a[v][0])+ abs(a[u][1]-a[v][1]));
         }
-
-        sort(edgeList.begin(), edgeList.end());
-
-        int totalCost= 0, edgesCount=0, n=points.size();
-
-        DSU dsu(n);
-
-        for(auto &[weight, i, j]: edgeList){
-            if(dsu.unite(i, j)){
-                totalCost+=weight;
-                edgesCount++;
-                if(edgesCount==n-1) break;
-            }
-        }
-        return totalCost;
+        return ans;
     }
 };
+/*
+Kruskal's:
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// Disjoint Set Union (DSU) with Path Compression and Union by Rank
+class DSU {
+private:
+    vector<int> parent;
+    vector<int> rank;
+
+public:
+    DSU(int n) {
+        parent.resize(n);
+        rank.assign(n, 0);
+        for (int i = 0; i < n; ++i) {
+            parent[i] = i;
+        }
+    }
+
+    int find(int i) {
+        if (parent[i] == i)
+            return i;
+        return parent[i] = find(parent[i]); // Path compression
+    }
+
+    bool unite(int i, int j) {
+        int root_i = find(i);
+        int root_j = find(j);
+
+        if (root_i != root_j) {
+            // Union by rank
+            if (rank[root_i] < rank[root_j]) {
+                swap(root_i, root_j);
+            }
+            parent[root_j] = root_i;
+            if (rank[root_i] == rank[root_j]) {
+                rank[root_i]++;
+            }
+            return true; // Successfully united (no cycle)
+        }
+        return false; // Already in the same component (cycle detected)
+    }
+};
+
+struct Edge {
+    int u, v, weight;
+    
+    // Sort edges by weight
+    bool operator<(const Edge& other) const {
+        return weight < other.weight;
+    }
+};
+
+int kruskalMST(int V, vector<Edge>& edges) {
+    sort(edges.begin(), edges.end()); // O(E log E)
+
+    DSU dsu(V);
+    int mst_weight = 0;
+    int edges_count = 0;
+
+    for (const auto& edge : edges) {
+        if (dsu.unite(edge.u, edge.v)) {
+            mst_weight += edge.weight;
+            edges_count++;
+            if (edges_count == V - 1) break; // MST complete
+        }
+    }
+
+    return (edges_count == V - 1) ? mst_weight : -1; // -1 if disconnected
+}
+*/
