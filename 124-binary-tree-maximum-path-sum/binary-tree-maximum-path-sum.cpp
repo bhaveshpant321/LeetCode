4d8c67dc3adbb30c19b14217ed58421fe842ec77
Bhavesh <142612199+bhaveshpant321@users.noreply.github.com>
2026-08-11 19:34:08 +0530

    Time: 3 ms (12.87%) | Memory: 29.3 MB (7.59%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -10,33 +10,25 @@
  * };
  */
 class Solution {
-    int maxSum= INT_MIN;    // Global tracker
+private:
+    int solve(TreeNode* root, int& res){
+        if(!root) return 0;
+        int l= solve(root->left, res);
+        int r= solve(root->right, res);
+
+        int temp= max(max(l,r)+ root->val, root->val);
+        int ans= max(temp, l+r+root->val);
+        res= max(res, ans);
+
+        return temp;
+    }
 public:
     int maxPathSum(TreeNode* root) {
-        // For any node u, there are 2 paths that include u
-        // 1. The rainbow: Starting from left subtree, goes through u, to right subtree
-        // 2. The line: Path comes from parent, picks u, and bigger of left or right subtree
-
-        // Strategy: Max tracker
-        // We use a global (or class level) variable maxSum to keep track of rainbow path, while our recursion function only returns the line to the parent
-
-        // Math: 
-        // For each node: Left gain= max(0, dfs(node->left)), and similar for right. We use 0 for "clipping" if the value becomes negative. (Instead of taking a negative sum path, better not take a path at all)
-        // The rainbow: node->val+ leftgain+ rightgain
-        // The line: node->val+ max(leftgain, rightgain); what we send to parent
-        solve(root);
-        return maxSum;
-    }
-private:
-    int solve(TreeNode* node){
-        if(!node)   return 0;
-
-        int leftgain= max(0, solve(node->left));
-        int rightgain= max(0, solve(node->right));
-
-        int currPathSum= node->val+ leftgain+ rightgain;    // rainbow sum
-        maxSum= max(maxSum, currPathSum);
-
-        return node->val+ max(leftgain, rightgain); // The Line
+        // path sum for a given node: node+ left+ right
+        // return: node+ max(left, right), or zero
+        if(!root) return 0;
+        int res= root->val;
+        solve(root, res);
+        return res;
     }
 };
