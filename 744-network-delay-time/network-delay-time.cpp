dd18e19ed672ddbf2d7dbb149ea861775b1c94e0
Bhavesh <142612199+bhaveshpant321@users.noreply.github.com>
2026-08-12 21:50:54 +0530

    Time: 72 ms (90.82%) | Memory: 44.5 MB (53.19%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,43 +1,40 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        // To start with times[k], which is essentially the source
-        // To find the minimum time it takes to reach the furthest node
-        // Dijkstra: We use a priority queue
-        // Initially, distance of each element from u will be infinity, update the distance 
-        // 1. Build the Adjacency List (Source -> {Neighbor, Weight})
-        
-        vector<vector<pair<int, int>>> adj(n + 1); // n+1 because nodes are 1-indexed
-        for (auto& edge : times) {
-            int u = edge[0];
-            int v = edge[1];
-            int w = edge[2];
-            adj[u].push_back({v, w});
+        // Step 1: Adjacency list> u : {v, weight}
+        vector<vector<pair<int, int>>> adj(n+1);
+        for(const auto& edge: times){
+            adj[edge[0]].push_back({edge[1], edge[2]});
         }
-        
-        vector<int> dist(n+1, 1e9);
-        dist[0]= 0; // 1 indexed array, so 0 will never be accessed
-        dist[k]= 0;
 
-        // Maintain a priority queue (min heap) with known distances
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        pq.push({0, k});    // distance, node
+        // Min heap (distance, node)
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int,int>>> pq;
+
+        vector<int> dist(n+1, 1e9); // 1-n
+
+        dist[k]=0;
+        pq.push({0, k});
 
         while(!pq.empty()){
-            auto [dis, u]= pq.top();
+            auto [d, u]= pq.top();
             pq.pop();
-            if(dis> dist[u]) continue;   // if distance is greater than previous min distance, discard
-            // Syntax to access neighbors:
-            for(auto& [v, weight] : adj[u]) { 
-                if(dist[u] + weight < dist[v]) {
-                    dist[v] = dist[u] + weight;
+        
+            // Skip outdated pair (lazy deletion)
+            if(d> dist[u]) continue;
+
+            for(const auto& [v, weight]: adj[u]){
+                if(dist[u]+weight< dist[v]){
+                    dist[v]= dist[u]+weight;
                     pq.push({dist[v], v});
                 }
             }
         }
+        int maxTime= 0;
+        for(int i=1; i<=n; i++){
+            if(dist[i]==1e9) return -1;
+            maxTime= max(maxTime, dist[i]);
+        }
+        return maxTime;
 
-        int delay= *max_element(dist.begin(), dist.end());
-
-        return delay==1e9? -1: delay;
     }
 };
