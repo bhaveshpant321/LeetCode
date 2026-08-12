e2cd862ddb640edfebddd64693f7e13fe1ddb1e4
Bhavesh <142612199+bhaveshpant321@users.noreply.github.com>
2026-08-12 12:55:51 +0530

    Time: 0 ms (100.00%) | Memory: 112.5 MB (62.38%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        // start from random index (here, 0 index), if gas becomes negative at any place, start from next
-        int n= gas.size();
-        int total= 0;   // gas-cost
-        int surplus= 0; // tank
-        int start=0;
-        for(int i=0; i<n; i++){
-            total+=gas[i]- cost[i];
-            surplus+= gas[i]-cost[i];
-            if(surplus<0){
-                surplus=0;
+        int n=gas.size();
+        int total_gas=0,total_cost=0;
+        
+        int curr_gas=0, start=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            //these two variable are to check if no case is possible
+            total_gas+=gas[i];
+            total_cost+=cost[i];
+            
+            
+            //for checking the current index
+            curr_gas+=gas[i]-cost[i];
+            
+            if(curr_gas<0)
+            {
+                //no index till i can fulfill the condition
                 start=i+1;
+                curr_gas=0;
+                //this index was useless so starting from next index
             }
         }
-        return total<0? -1: start;
+        
+        return (total_gas<total_cost)?-1:start;     
+        
+        
     }
 };
