c61533ed31b32a72d72ce1d28cb87294d818c391
Bhavesh <142612199+bhaveshpant321@users.noreply.github.com>
2026-08-12 12:34:40 +0530

    Time: 33 ms (40.04%) | Memory: 47.6 MB (39.08%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,40 +1,31 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        // Structure:  MaxHeap, Algo: Greedy, Pattern: Bottleneck/Frequency
-        // Approach: Since we have a cooling period of n, we don't just pop most frequent and push it back, we have to make it "unavailable" for n units of time
-        priority_queue<int> heap;   // To store just the frequencies. As we don't need the name of the process
-        // Calculating the frequency
-        int chars[26]={0};
-        for(auto task: tasks){
-            chars[task-'A']++;
+        vector<int> frequency(26, 0);
+        for(auto t: tasks){
+            frequency[t-'A']++;
         }
-        // Fill the heap based on frequency
+        priority_queue<int> pq;
         for(int i=0; i<26; i++){
-            if(chars[i]>0){
-                heap.push(chars[i]);
-            }
+            if(frequency[i]) pq.push(frequency[i]);
         }
-
-        queue<pair<int, int>> waitQueue;   // Wait queue to store processed process for n time, push back to heap once time is up
         int time=0;
-        
-        while(!heap.empty() || !waitQueue.empty()){
-            time++;
-            if(!heap.empty()){
-                int remaining= heap.top()-1;    // Calculates the remaining time out of the process with highest frequency. Heap just contains frequencies, not the processes themselves.
-                heap.pop();
-                if(remaining>0){
-                    waitQueue.push({remaining, time+n});
-                }
+        while(!pq.empty()){
+            vector<int> remain;
+            int cycle= n+1; // after A task, there will be n more tasks, so cooldown period= n+1
+            while(cycle && !pq.empty()){
+                int max_freq= pq.top();
+                pq.pop();
+                if(max_freq>1) remain.push_back(max_freq-1);
+                time++;
+                cycle--;
             }
-            // Check if any task in waitQueue is ready to be pushed back
-            if(!waitQueue.empty() && waitQueue.front().second==time){
-                heap.push(waitQueue.front().first);
-                waitQueue.pop();
+            for(int count: remain){
+                pq.push(count);
             }
+            if(pq.empty()) break;
+            time+=cycle;
         }
         return time;
-
     }
 };
