04632779cf90fa888b926c093ce9910ff3d996de
Bhavesh <142612199+bhaveshpant321@users.noreply.github.com>
2026-09-02 16:13:39 +0530

    Time: 83 ms (47.98%) | Memory: 88.9 MB (49.13%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        if(nums.empty())  return 0;
+        // Put the numbers in a set, start checking only for valid first numbers
+        // Eg in (100, 4, 200, 1, 3, 2) 1 is valid first number as 0 isn't there, but 2 isn't as 1 is there
         unordered_set<int> numSet(nums.begin(), nums.end());
-        int count=0;
-        for(int num: numSet){
-            //Check if a number smaller than current by 1 exists:
-            if(numSet.count(num-1)==0){
-                int currCount=1;    //As current element will be the first element of the sequence
-                while(numSet.count(num+currCount)){
-                    currCount++;
-                }
-                count= max(count, currCount);
+        int longest=0;
+        for(int n: numSet){
+            if(!numSet.count(n-1)){ // found valid first
+                int len=1;
+                while(numSet.count(n+len)) len++;
+                longest= max(longest, len);
             }
         }
-        return count;
+        return longest;
     }
 };
