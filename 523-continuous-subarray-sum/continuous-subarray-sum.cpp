e0bf5ea5271fc1dbcefa422c00dd0843797ed481
Bhavesh <142612199+bhaveshpant321@users.noreply.github.com>
2026-09-02 16:07:45 +0530

    Time: 76 ms (67.30%) | Memory: 146.3 MB (57.15%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.cpp
+++ 523-continuous-subarray-sum/continuous-subarray-sum.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        // Property: 2 prefix sums: Si and Sj, and Si+Sj(mod k)==0; then Sj (mod k)= Si (mod k)
-        // Keep a running prefix sum, store prefix sum (mod k) in the hashmap<remainder, index>
-        // If you see the same remainder again, it means the elements added between the first time you saw that remainder and now sum up to a multiple of k
-        // If difference in indices>=2, return true
+        // prefix sum+ hashmap
+        unordered_map<int, int> map;
+        map[0]=-1;    // Why? So that we can return true when runningSum%k=0
+        int runningSum=0;
 
-        int n= nums.size();
-        vector<int> prefix(n);
-        prefix[0]= nums[0];
-        for(int i=1; i<n; i++){
-            prefix[i]= prefix[i-1]+ nums[i];
-        }
-
-        unordered_map<int, int> map; // remainder, index
-        map[0]= -1;     // Very important line
-        //if a prefix sum itself is a multiple of $k$ starting from the very first element, you need a "virtual" index at -1 to satisfy the length>= 2 condition.
-
-        for(int i=0; i<n; i++){
-            int rem= prefix[i]%k;
-            if(map.count(rem)){
-                if(i-map[rem]>=2)return true;
-            }else map[rem]= i;
+        for(int i=0; i<nums.size(); i++){
+            runningSum+= nums[i];
+            if(k!=0) runningSum= runningSum%k;
+            if(map.count(runningSum)){
+                if(i- map[runningSum]>1) return true;
+            }else map[runningSum]= i;
         }
         return false;
     }
