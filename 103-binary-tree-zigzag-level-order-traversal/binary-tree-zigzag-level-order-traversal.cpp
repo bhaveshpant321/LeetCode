6feb0c51c6dae61e0b0f647f1cb0a177ced15e6f
Bhavesh <142612199+bhaveshpant321@users.noreply.github.com>
2026-09-05 17:49:14 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (48.96%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -12,30 +12,33 @@
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>> res;
-        if(!root)   return res;
+        if (!root) return {};
         
+        vector<vector<int>> result;
         queue<TreeNode*> q;
         q.push(root);
-        int level=1;
-        while(!q.empty()){
-            vector<int> currLevel;
-            int size= q.size();
-            for(int i=0; i< size; i++){
-                TreeNode* node= q.front();
+        bool leftToRight = true;
+        
+        while (!q.empty()) {
+            int size = q.size();
+            vector<int> currentLevel(size);
+            
+            for (int i = 0; i < size; ++i) {
+                TreeNode* node = q.front();
                 q.pop();
-
-                if(node->left) q.push(node->left);
-                if(node->right) q.push(node->right);
-
-                currLevel.push_back(node->val);
+                
+                // Calculate position based on current direction
+                int index = leftToRight ? i : (size - 1 - i);
+                currentLevel[index] = node->val;
+                
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
             }
-
-            if(level%2==0) reverse(currLevel.begin(), currLevel.end());
-            res.push_back(currLevel);
-            level++;
+            
+            result.push_back(move(currentLevel));
+            leftToRight = !leftToRight;
         }
-
-        return res;
+        
+        return result;
     }
 };
