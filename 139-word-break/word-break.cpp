cc73e4f3723a2908130c08ca8575d5c3b1b9a3e3
Bhavesh <142612199+bhaveshpant321@users.noreply.github.com>
2026-09-07 22:21:37 +0530

    Time: 3 ms (71.56%) | Memory: 12.5 MB (64.68%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        vector<bool> dp(s.size()+1);    // dp[i] is true if substring [0->i-1] can be segmented into dictionary words
-        dp[0]= true;
+        // try every index and see if it's possible to make from all the possible 
         unordered_set<string> dict(wordDict.begin(), wordDict.end());
-        int n= s.size();
-        for(int i=1; i<=n; i++){
-            for(int j=0; j<i; j++){
-                //If s[0...j] is valid AND s[j...i] is a word
+        vector<bool> dp(s.size()+1, false);
+        dp[0]= true;
+        for(int i=1; i<= s.size(); i++){
+            for(int j=i-1; j>=0; j--){   // can definitely go forward as well, but moving backwards has real life optimization advantages, as we'll be first checking smaller substrings in dict and dicts in real life would normally have strings of small size. 
+            // Optimization: While inserting in the unordered set, we can also keep note of the max len of the substring and use that as the delimiter instead of 0 for less loop lookups
+
                 if(dp[j] && dict.count(s.substr(j, i-j))){
-                    dp[i]=true; break;
+                    dp[i]= true;
+                    break;
                 }
             }
         }
-        return dp[n];
+        return dp[s.size()];
     }
 };
