6fdaa9c2d98b0587fe971dc4abd4713d9a0a15d5
Bhavesh <142612199+bhaveshpant321@users.noreply.github.com>
2026-09-08 10:26:34 +0530

    Time: 0 ms (100.00%) | Memory: 25.9 MB (77.65%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n= height.size();
-        vector<int> leftMax(n, 0), rightMax(n, 0);
-        // Build the leftMax array to store the leftTallest wall
-        for(int i=1; i<n; i++){
-            leftMax[i]= max(leftMax[i-1], height[i-1]);
-        }
-
-        for(int i=n-2; i>=0; i--){
-            rightMax[i]= max(rightMax[i+1], height[i+1]);
-        }
-
-        // Now find the actual water:
+        // for each index i, the max water that can be held there is equivalent to min of max height from both left and right- hieght of the block itself
+        // Trapped rainwater= sum of all
+        int left=0, right= height.size()-1;
+        int leftMax= height[left], rightMax= height[right];
         int water=0;
-        for(int i=1; i<n-1; i++){
-            water+= max(0, min(leftMax[i], rightMax[i])- height[i]);
+
+        while(left<right){
+            if(leftMax< rightMax){
+                left++;
+                leftMax= max(leftMax, height[left]);
+                water+=leftMax-height[left];
+            }else{
+                right--;
+                rightMax= max(rightMax, height[right]);
+                water+=rightMax- height[right];
+            }
         }
         return water;
     }
