50e2aa4810d90cd000adc651afbe96186176c8d7
Bhavesh <142612199+bhaveshpant321@users.noreply.github.com>
2026-09-10 06:21:50 +0530

    Time: 0 ms (100.00%) | Memory: 23.6 MB (98.68%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -4,12 +4,9 @@ public:
         int j=0;    // pointer for placing next non-zero element
         for(int i=0; i<nums.size(); i++){
             if(nums[i]!=0){
-                if(i!=j) nums[j]= nums[i];
+                if(i!=j) swap(nums[j], nums[i]);
                 j++;
             }
-        }
-        while(j<nums.size()){
-            nums[j++]=0;
         }
     }
 };
