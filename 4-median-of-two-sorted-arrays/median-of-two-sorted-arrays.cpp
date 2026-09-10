4230c75714def220693c894bf78ee364bf959543
Bhavesh <142612199+bhaveshpant321@users.noreply.github.com>
2026-09-10 10:14:33 +0530

    Time: 0 ms (100.00%) | Memory: 95.1 MB (79.67%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n1 = nums1.size(), n2 = nums2.size();
-        if (n1 > n2) return findMedianSortedArrays(nums2, nums1);
+        // O(log(min(m, n)));
 
-        int l = 0, r = n1;
-        int half = (n1 + n2 + 1) / 2;
+        int m= nums1.size(), n= nums2.size();
+        if(m>n) return findMedianSortedArrays(nums2, nums1);
 
-        while (l <= r) {
-            int i = l + (r - l) / 2;
-            int j = half - i;
+        int low=0, high= m;
+        
+        while(low<=high){
+            int cut1= low+ (high-low)/2;
+            int cut2= (m+n+1)/2- cut1;
 
-            // Use i-1 and j-1 for the left side
-            double left1 = (i == 0) ? -1e18 : nums1[i - 1]; 
-            double right1 = (i == n1) ? 1e18 : nums1[i];
-            double left2 = (j == 0) ? -1e18 : nums2[j - 1];
-            double right2 = (j == n2) ? 1e18 : nums2[j];
+            int l1= cut1==0? INT_MIN: nums1[cut1-1];
+            int l2= cut2==0? INT_MIN: nums2[cut2-1];
+            
+            int r1= cut1==m? INT_MAX: nums1[cut1];
+            int r2= cut2==n? INT_MAX: nums2[cut2];
 
-            if (left1 <= right2 && left2 <= right1) {
-                // Odd total length
-                if ((n1 + n2) % 2 != 0) return max(left1, left2);
-                // Even total length
-                return (max(left1, left2) + min(right1, right2)) / 2.0;
-            } 
-            else if (left1 > right2) {
-                r = i - 1;
-            } else {
-                l = i + 1;
-            }
-        }
+            if(l1<=r2 && l2<=r1){
+                // found correct pos
+                if((m+n)%2==1) return max(l1, l2);
+                else return (max(l1, l2)+min(r1, r2))/2.0;
+            }else if(l1> r2) high= cut1-1;
+            else low= cut1+1;
+        }    
         return 0.0;
     }
 };
